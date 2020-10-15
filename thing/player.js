7e2dc0005e73f553e190c980127fa2ebbb2bb0ba
fetails <45269692+fetails@users.noreply.github.com>
2020-10-15 19:47:39 +0200

    s

--- thing/player.js
+++ thing/player.js
@@ -5,6 +5,9 @@ class c_player
         this.velocity = 0.000;
         this.pos = { x: 0, y: 0 };
         this.size = { x: 40, y: 40 };
+
+        this.previous_position = [ ];
+        this.last_update = 0;
     }
 
     move_to( _x, _y )
@@ -13,9 +16,26 @@ class c_player
         this.pos.y = _y;
     }
 
-    render_player( ctx )
+    render_player( ctx, track = true )
     {
+        if( track )
+        {
+            for( var i = 0; i < this.previous_position.length; i++ )
+                draw_filled_rect( ctx, this.previous_position[ i ].x, this.previous_position[ i ].y, 40, 40, "#2d2d2d" );
+    
+            if( Date.now() > this.last_update + 7 )
+            {
+                this.last_update = Date.now();
+                this.previous_position.push( { x: this.pos.x, y: this.pos.y } );
+            };
+    
+            if( this.previous_position.length > 15 )
+                this.previous_position.shift();
+        }
+        
+
         draw_filled_rect( ctx, this.pos.x, this.pos.y, 40, 40, "#2E9AFE" );
+        
     }
 
     set_spawn( _width, _height )
